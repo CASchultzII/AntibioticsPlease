47fe1eff5f49aa6eb23f242289f5e213c5ff30c4
CASchultzII <c.a.schultz.ii@gmail.com>
2016-10-13 18:06:33 -0600

    Finished most of Game components..

--- Antibiotics Please/Assets/Scripts/Patient/Patient.cs
+++ Antibiotics Please/Assets/Scripts/Patient/Patient.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using System;
 
 public class Patient {
 
-    public Patient() {
+    public Patient(Sprite[] portraits) {
         System.Random rand = new System.Random();
+
+        sprite = portraits[rand.Next(portraits.Length)];
+
         bacterial = rand.NextDouble() <= Constants.CHANCE_BACTERIAL;
 
         // resistances
@@ -19,6 +23,7 @@ public class Patient {
     private bool[] resistance = new bool[3];
     private Health.state state;
     private int doses;
+    private Sprite sprite;
 
     public bool bacterialInfection {
         get { return bacterial; }
@@ -37,6 +42,29 @@ public class Patient {
     public Health.state apparentHealth {
         get { return state; }
         set { state = value; }
+    }
+
+    public float healthDouble {
+        get {
+            switch (state) {
+                case Health.state.HEALTHY:
+                    return 1.0F;
+                case Health.state.SICKLY:
+                    return 0.75F;
+                case Health.state.DISEASED:
+                    return 0.5F;
+                case Health.state.MORBIDLY_ILL:
+                    return 0.25F;
+                case Health.state.DECEASED:
+                    return 0.0F;
+                default:
+                    throw new Exception("This should not happen...");
+            }
+        }
+    }
+
+    public Sprite portrait {
+        get { return sprite; }
     }
 
     public int dosesRemaining {
---
+++ Antibiotics Please/Assets/Scripts/UIManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public void ChangeScene(string scene) {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+}
